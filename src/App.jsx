d8fe1222acf3422c339e0afb6c8b8b25f3489d77
18531081514 <1359063021@qq.com>
2025-06-10 18:15:46 +0800

    refactor(App): 移除 InfoModal 组件并调整 App 布局
    - 删除了 InfoModal 组件的导入和使用
    - 移除了 App 组件中的 !hasWords 条件渲染
    - 调整了 RefreshButton、SaveButton 和 ShareQR 组件的布局
    - 优化了 App 组件的代码结构

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { lazy, Suspense, useEffect, useState } from 'react';
+import { lazy, Suspense, useState } from 'react';
 import styled from 'styled-components';
 
 import { getQueryValue } from './utils';
 import Loading from './components/Loading';
 import Words from './assets/words';
 
-// const Card = lazy(() => import('./components/Card'));
 const Card = lazy(() => import('./components/Card'));
 const LoadingWords = lazy(() => import('./components/LoadingWords'));
 const StartButton = lazy(() => import('./components/StartButton'));
@@ -14,7 +13,6 @@ const SaveButton = lazy(() => import('./components/SaveButton'));
 const ShareQR = lazy(() => import('./components/ShareQR'));
 const Header = lazy(() => import('./components/Header'));
 
-const InfoModal = lazy(() => import('./components/InfoModal'));
 const MetooButton = styled(StartButton)`
   z-index: 998;
   position: fixed;
@@ -24,27 +22,31 @@ const MetooButton = styled(StartButton)`
   margin-left: -7rem;
   animation-delay: ${({ wordCount }) => `${wordCount * 0.3}s`};
 `;
+
 const wordsIdx = getQueryValue('idx');
 const hasWords = wordsIdx !== '';
+
 const App = () => {
   let count = wordsIdx !== '' ? Words[wordsIdx].length : 0;
 
   const [start, setStart] = useState(hasWords);
   const [loading, setLoading] = useState(!hasWords);
+
   const handleStart = () => {
     setStart(true);
     setLoading(true);
   };
+
   const handleDone = () => {
     setLoading(false);
   };
+
   const handleUpdate = () => {
     setLoading(true);
   };
 
   return (
     <Suspense fallback={<Loading />}>
-      {!hasWords && <InfoModal />}
       {start && !loading && !hasWords && <ShareQR />}
       <RefreshButton visible={start && !loading && !hasWords} handleUpdate={handleUpdate} />
       <SaveButton visible={start && !loading && !hasWords} />
@@ -64,4 +66,5 @@ const App = () => {
     </Suspense>
   );
 };
+
 export default App;
